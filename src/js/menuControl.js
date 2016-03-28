677b2fbbbd36dac5ceb5d1c2d8bcebfbedf437a6
chris <chriseparsons@hotmail.co.uk>
2016-03-28 21:53:08 +0100

    Tools html page

--- src/js/menuControl.js
+++ src/js/menuControl.js
@@ -16,17 +16,19 @@
     });
 
     var tabs =
-    {
-        name: "Tools and Frameworks",
-        description: "The tools used, like Selenium, UI Automation, TestComplete, Sikuli, RestAssured, "+
-        "RestSharp, Mocha, JMeter, Jasmine/Karma and some others.",
-        jasmine: "This is Jasmine text",
-        mocha: "This is Mocha text",
-        selenium: "This is Selenium text",
-        uiautomation: "This is UIAutomation text",
-        testcomplete: "This is TestComplete text",
-        sikuli: "This is Sikuli text",
-        restassuredsharp: "This is RestAssured text",
-        jmeter: "This is JMeter text"
-    };
+    [
+        {
+            name: "Tools and Frameworks",
+            description: "The tools used, like Selenium, UI Automation, TestComplete, Sikuli, RestAssured, "+
+            "RestSharp, Mocha, JMeter, Jasmine/Karma and some others.",
+            jasmine: "This is Jasmine text",
+            mocha: "This is Mocha text",
+            selenium: "This is Selenium text",
+            uiautomation: "This is UIAutomation text",
+            testcomplete: "This is TestComplete text",
+            sikuli: "This is Sikuli text",
+            restassuredsharp: "This is RestAssured text",
+            jmeter: "This is JMeter text"
+        }
+    ];
 })();
