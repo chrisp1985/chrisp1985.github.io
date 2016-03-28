53e124203c0c31df01cff9092a11775a8dc4fefe
chris <chriseparsons@hotmail.co.uk>
2016-03-28 21:59:22 +0100

    Attempt to rectify incorrect angular

--- src/js/menuControl.js
+++ src/js/menuControl.js
@@ -18,17 +18,17 @@
     var tabs =
     [
         {
-            name: "Tools and Frameworks",
-            description: "The tools used, like Selenium, UI Automation, TestComplete, Sikuli, RestAssured, "+
+            name:"Tools and Frameworks",
+            description:"The tools used, like Selenium, UI Automation, TestComplete, Sikuli, RestAssured, "+
             "RestSharp, Mocha, JMeter, Jasmine/Karma and some others.",
-            jasmine: "This is Jasmine text",
-            mocha: "This is Mocha text",
-            selenium: "This is Selenium text",
-            uiautomation: "This is UIAutomation text",
-            testcomplete: "This is TestComplete text",
-            sikuli: "This is Sikuli text",
-            restassuredsharp: "This is RestAssured text",
-            jmeter: "This is JMeter text"
+            jasmine:"This is Jasmine text",
+            mocha:"This is Mocha text",
+            selenium:"This is Selenium text",
+            uiautomation:"This is UIAutomation text",
+            testcomplete:"This is TestComplete text",
+            sikuli:"This is Sikuli text",
+            restassuredsharp:"This is RestAssured text",
+            jmeter:"This is JMeter text"
         }
     ];
 })();
