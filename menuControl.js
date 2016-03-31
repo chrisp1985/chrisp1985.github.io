957aba812affb84db406d3dee7963cb40ef54eb3
chris <chriseparsons@hotmail.co.uk>
2016-03-31 20:08:03 +0100

    syntax stuff

---
+++ menuControl.js
@@ -0,0 +1,34 @@
+(function() {
+    var app = angular.module("myModule",[]);
+
+    app.controller("myCtrl", function() {
+        this.tab = 1;
+
+        this.selects = tabs;
+
+        this.setTab = function(newValue){
+          this.tab = newValue;
+        };
+
+        this.isSet = function(tabName){
+          return this.tab === tabName;
+        };
+    });
+
+    var tabs =
+    [
+        {
+            name:"Jasmine Text",
+            description:"Jasmine is a unit testing tool framework that allows developers to verify and validate Javascript components."
+        },
+        {
+            name:"Selenium Text",
+            description:"Selenium is a framework that allows a user to interact with elements in the DOM to test a website."
+        },
+        {
+            name:"UI Automation Text",
+            description:"UI Automation is an automation framework developed by Microsoft to automate actions within a desktop application.",
+            examples: "This is some code"
+        }
+    ];
+})();
