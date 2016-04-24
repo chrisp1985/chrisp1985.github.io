e7fc9f244c5d07d8c54c127da41c7f2df2c24291
chris <chriseparsons@hotmail.co.uk>
2016-04-24 14:46:15 +0100

    Added lots of 2 pages for ui-view.

--- menuControl2.js
+++ menuControl2.js
@@ -1,7 +1,27 @@
 (function() {
-    var app = angular.module("myModule",[]);
+    var app = angular.module("myModule",['ui.router']);
 
-    app.controller("myCtrl", function() {
+    app.config(function($stateProvider, $urlRouterProvider){
+
+        // For any unmatched url, send to /home
+        $urlRouterProvider.otherwise("/home")
+
+        $stateProvider
+            .state('home', {
+                url: "/home",
+                templateUrl: "home2.html"
+            })
+            .state('tools', {
+                url: "/tools",
+                templateUrl: "tools2.html"
+            })
+            .state('this_site', {
+                url: "/this_site",
+                templateUrl: "this_site2.html"
+            })
+    })
+
+    .controller("myCtrl", function() {
         this.tab = 1;
 
         this.selects = tabs;
